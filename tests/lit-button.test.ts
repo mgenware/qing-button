2587d82ae8efe042b45be583301815ca6f9075fe
Mgen <m@mgenware.com>
2020-01-08 16:37:25 +0800

    Convert test files to typescript

--- tests/lit-button.test.ts
+++ tests/lit-button.test.ts
@@ -1,21 +1,22 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { html, fixture, expect, oneEvent } from '@open-wc/testing';
 import '../dist/main';
 
 it('Content slot', async () => {
-  const ele = await fixture(html`
+  const el = await fixture(html`
     <lit-button><p>test</p></lit-button>
   `);
 
-  expect(ele.innerHTML).to.eq('<p>test</p>');
+  expect(el.innerHTML).to.eq('<p>test</p>');
 });
 
 it('click event', async () => {
-  const ele = await fixture(html`
+  const el = (await fixture(html`
     <lit-button></lit-button>
-  `);
+  `)) as HTMLElement;
 
-  const listener = oneEvent(ele, 'click');
-  ele.shadowRoot.querySelector('button').click();
+  const listener = oneEvent(el, 'click');
+  el.shadowRoot!.querySelector('button')!.click();
   const { detail, composed, bubbles } = await listener;
   expect(detail).to.eq(0);
   expect(composed).to.eq(true);
