a8793a38b578f28bcd8af0058e4ef58a4be2d434
Mgen <m@mgenware.com>
2020-06-20 01:33:37 +0800

    Fix missing CSSResultArray typing causing problems overriding style property

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,50 @@
-import { html, customElement, css, property, LitElement } from 'lit-element';
+import {
+  html,
+  customElement,
+  css,
+  property,
+  LitElement,
+  CSSResultArray,
+} from 'lit-element';
 
 @customElement('lit-button')
 export default class LitButton extends LitElement {
-  static get styles() {
-    return css`
-      :host {
-        display: inline-block;
-      }
-      button {
-        margin: 0 0 1rem 0;
-        background-color: #e7e7e7;
-        color: black;
-        border: 0;
-        border-radius: 0;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        cursor: pointer;
-        font-size: 1rem;
-        padding: 0.6rem 0.9rem;
-        transition: all 0.3s ease 0s;
-      }
-      button:hover {
-        opacity: 0.8;
-      }
-      button:active {
-        filter: brightness(80%);
-      }
-      button:disabled {
-        pointer-events: none;
-        opacity: 0.6;
-      }
-      button:focus {
-        box-shadow: inset 0 0 0 0.2rem var(--button-outline-color, #8dc3eb);
-        outline: none;
-      }
-    `;
+  static get styles(): CSSResultArray {
+    return [
+      css`
+        :host {
+          display: inline-block;
+        }
+        button {
+          margin: 0 0 1rem 0;
+          background-color: #e7e7e7;
+          color: black;
+          border: 0;
+          border-radius: 0;
+          text-align: center;
+          text-decoration: none;
+          display: inline-block;
+          cursor: pointer;
+          font-size: 1rem;
+          padding: 0.6rem 0.9rem;
+          transition: all 0.3s ease 0s;
+        }
+        button:hover {
+          opacity: 0.8;
+        }
+        button:active {
+          filter: brightness(80%);
+        }
+        button:disabled {
+          pointer-events: none;
+          opacity: 0.6;
+        }
+        button:focus {
+          box-shadow: inset 0 0 0 0.2rem var(--button-outline-color, #8dc3eb);
+          outline: none;
+        }
+      `,
+    ];
   }
 
   @property({ type: Boolean }) disabled = false;
