5ce5190b480711fec22509995bcb9e2742726b6f
Mgen <m@mgenware.com>
2021-02-28 19:30:19 +0800

    Add modern-normalize to demo

--- demo/main.js
+++ demo/main.js
@@ -7,31 +7,35 @@ export default class ExampleApp extends LitElement {
   render() {
     return html`
       <div id="main">
-        <qing-button id="btn" @click=${() => alert('Hello world')}>Default</qing-button>
-        <br />
-        <qing-button class="green">Green</qing-button> <br />
-        <qing-button class="small">Small</qing-button> <br />
-        <qing-button class="black large">Black, Large</qing-button> <br />
-        <qing-button class="blue small round colored-border"
-          >Blue, small, round, colored-border</qing-button
-        >
-        <br />
-        <qing-button class="blue small round colored-border" disabled>
-          Blue, small, round, colored-border, disabled
-        </qing-button>
-        <br />
-        <qing-button disabled>Disabled</qing-button>
-        <br />
+        <p>
+          <qing-button>Default</qing-button>
+          <qing-button btnStyle="primary">Primary</qing-button>
+          <qing-button btnStyle="success">Success</qing-button>
+          <qing-button btnStyle="warning">Warning</qing-button>
+          <qing-button btnStyle="danger">Danger</qing-button>
+          <qing-button class="custom-styles">Custom styles</qing-button>
+          <qing-button disabled>Disabled</qing-button>
+        </p>
         <qing-button autofocus>Auto-focused</qing-button>
         <br />
         <qing-button href="http://google.com">href = google.com</qing-button>
-        <p>A <qing-button class="btn-in-paragraph">button</qing-button> in a paragraph.</p>
+        <p>
+          A <qing-button class="btn-in-paragraph">button</qing-button> in a paragraph with an icon
+          <qing-button>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path
+                d="M12 9.229c.234-1.12 1.547-6.229 5.382-6.229 2.22 0 4.618 1.551 4.618 5.003 0 3.907-3.627 8.47-10 12.629-6.373-4.159-10-8.722-10-12.629 0-3.484 2.369-5.005 4.577-5.005 3.923 0 5.145 5.126 5.423 6.231zm-12-1.226c0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-7.962-9.648-9.028-12-3.737-2.338-5.262-12-4.27-12 3.737z"
+              />
+            </svg> </qing-button
+          >.
+        </p>
+        <qing-button id="themes" @click=${this.switchThemes}>Switch themes</qing-button><br />
+        <h2>Button group</h2>
         <p class="btn-group">
           <qing-button>Windows</qing-button>
           <qing-button>macOS</qing-button>
           <qing-button>Linux</qing-button>
         </p>
-        <qing-button id="themes" @click=${this.switchThemes}>Switch themes</qing-button><br />
         <h2>CSS variables</h2>
         <qing-button id="button-outline">--button-outline-color</qing-button>
         <h2>Selected</h2>
@@ -68,43 +72,29 @@ ExampleApp.styles = css`
   qing-button {
     margin-bottom: 1rem;
   }
-  qing-button.green::part(button) {
+  qing-button[btnStyle='success']::part(button) {
     background-color: #4caf50;
     color: white;
-  } /* Green */
-  qing-button.blue::part(button) {
+  }
+  qing-button[btnStyle='primary']::part(button) {
     background-color: #008cba;
     color: white;
-  } /* Blue */
-  qing-button.red::part(button) {
+  }
+  qing-button[btnStyle='danger']::part(button) {
     background-color: #f44336;
     color: white;
-  } /* Red */
-  qing-button.gray::part(button) {
-    background-color: #e7e7e7;
-    color: black;
-  } /* Gray */
-  qing-button.black::part(button) {
-    background-color: #555555;
+  }
+  qing-button[btnStyle='warning']::part(button) {
+    background-color: #ffae00;
     color: white;
-  } /* Black */
-  qing-button.small::part(button) {
-    font-size: 0.75rem;
-    padding: 0.5rem 0.75rem;
-    margin-bottom: 0.75rem;
   }
-  qing-button.large::part(button) {
-    font-size: 1.25rem;
-    padding: 1rem 1.25rem;
-    margin-bottom: 1.25rem;
-  }
-  qing-button.round::part(button) {
-    border-radius: 0.75rem;
-  }
-  qing-button.colored-border::part(button) {
+  qing-button.custom-style::part(button) {
     border: 4px solid yellowgreen;
     border-radius: 10px;
+    background-color: #50009b;
+    color: white;
   }
+
   .btn-in-paragraph {
     margin-left: 0.25rem;
     margin-right: 0.25rem;
