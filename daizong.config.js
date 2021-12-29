e68abe6769f4eecb51784c8cf58a0280d8e3d3d6
Mgen <m@mgenware.com>
2021-12-29 15:12:19 +0800

    Move to @web/test-runner-playwright

--- daizong.config.js
+++ daizong.config.js
@@ -1,39 +1,32 @@
+const runTestCmd = 'wtr dist_tests/**/*.test.js --node-resolve --playwright --browsers webkit';
+
 export default {
   // Starts development mode: watches and compiles all source files including tests.
   dev: {
     run: ['#clean', 'tsc -b tests -w'],
     envGroups: ['development'],
   },
-  serve: {
-    run: ['web-dev-server --open demo/ --node-resolve --watch'],
-  },
 
   // Runs tests (you need to build the project first).
-  t: {
-    run: 'web-test-runner dist_tests/**/*.test.js --node-resolve',
-  },
+  t: runTestCmd,
   // Runs tests in watch mode (you need to build the project first).
-  tw: {
-    run: 'web-test-runner dist_tests/**/*.test.js --node-resolve --watch',
-  },
+  tw: `${runTestCmd} --watch`,
 
-  // Cleans, lints, compiles sources and runs tests.
+  // Cleans, lints, compiles and runs tests.
   build: {
     run: ['#clean', 'tsc -b tests', '#lint', '#t'],
     envGroups: ['production'],
   },
 
-  // Deletes compiled files, auto triggered by `yarn r dev` or `yarn r build`.
+  // Deletes compiled files (auto triggered by `npm run r dev` or `npm run r build`).
   clean: {
     run: {
       del: ['dist', 'dist_tests'],
     },
   },
 
-  // Lints the project using ESLint, auto triggered by `yarn r build`.
-  lint: {
-    run: 'eslint --max-warnings 0 --ext .ts src/ tests/',
-  },
+  // Lints the project using ESLint (auto triggered by `npm run r build`).
+  lint: 'eslint --max-warnings 0 --ext .ts src/ tests/',
 
   _: {
     envGroups: {
